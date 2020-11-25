ecd1ebcec850ad679164c737c303dd9cd4b8beed
volker-baecker <volker.baecker@mri.cnrs.fr>
2020-11-25 12:50:17 +0100

    changed from neubiaswg5 to biaflows

--- wrapper.py
+++ wrapper.py
@@ -3,8 +3,8 @@ from subprocess import call
 
 from cytomine.models import Job
 
-from neubiaswg5 import CLASS_OBJSEG
-from neubiaswg5.helpers import NeubiasJob, prepare_data, upload_data, upload_metrics
+from biaflows import CLASS_OBJSEG
+from biaflows.helpers import NeubiasJob, prepare_data, upload_data, upload_metrics
 
 
 def main(argv):
